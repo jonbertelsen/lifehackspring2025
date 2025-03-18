1ee71fbc00c9d7e0a5691c0beeecc1e1e1249eaa
Jon Bertelsen <jobe@cphbusiness.dk>
2025-03-18 15:11:53 +0100

    add exception class

---
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -0,0 +1,21 @@
+package app.exceptions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseException extends Exception
+{
+    private static final Logger LOGGER = Logger.getLogger(DatabaseException.class.getName());
+
+    public DatabaseException(String userMessage)
+    {
+        super(userMessage);
+        LOGGER.log(Level.WARNING, userMessage);
+    }
+
+    public DatabaseException(String userMessage, String systemMessage) {
+        super(userMessage);
+        LOGGER.log(Level.WARNING, userMessage);
+        LOGGER.log(Level.WARNING, "errorMessage: " + systemMessage);
+    }
+}
