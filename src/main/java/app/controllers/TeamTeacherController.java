ef752b5f7289981bb8dfa42ac8b26e6e9bacbae8
KongCPH <112859698+KongCPH@users.noreply.github.com>
2025-03-18 19:58:26 +0100

    philosopher project

--- src/main/java/app/controllers/TeamTeacherController.java
+++ src/main/java/app/controllers/TeamTeacherController.java
@@ -1,16 +1,39 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import app.persistence.TeamTeacherMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TeamTeacherController {
+
+    private static ConnectionPool connectionPool;
+
+
+    public TeamTeacherController(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
 
     public static void routes(Javalin app) {
         app.get("/team-teacher/index", ctx -> teamTeacherFrontHome(ctx));
+       // app.get("/ask", ctx -> ctx.render("answer.html"));
+        app.post("/ask", ctx -> getAnswer(ctx));
     };
 
     private static void teamTeacherFrontHome(@NotNull Context ctx) {
         ctx.render("team-teacher/index.html");
     }
+
+    private static void getAnswer(Context ctx){
+        String question = ctx.formParam("question");
+        List<Integer> keywordIDs = TeamTeacherMapper.getKeyWordIDs(question, connectionPool);
+
+    }
+
+
 }
--- src/main/java/app/persistence/TeamTeacherMapper.java
+++ src/main/java/app/persistence/TeamTeacherMapper.java
@@ -1,6 +1,35 @@
 package app.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
 public class TeamTeacherMapper {
 
+
+    public static List<Integer> getKeyWordIDs(String input, ConnectionPool connectionPool){
+        List<Integer> keywordIDs = new ArrayList<>();
+        String[] words = input.toLowerCase().split("\\s+");
+        String query = "SELECT keyword FROM keywords";
+        try (Connection conn = connectionPool.getConnection()){
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery();
+             while (rs.next()) {
+                 String keyword = rs.getString("keyword").toLowerCase();
+                 int keywordID = rs.getInt("id");
+                 for (String word : words) {
+                     if (word.equals(keyword)) {
+                         keywordIDs.add(keywordID);
+                         break; // Undgår at tilføje samme keyword flere gange
+                     }
+                 }
+             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return keywordIDs;
+    }
 
 }
