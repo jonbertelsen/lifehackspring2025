6c67d55e8024400f858a0931ef2ccaab027858a2
NicolaiKristiansen <nicolaikristiansen223@gmail.com>
2025-03-18 15:57:33 +0100

    We have + new Soda class + A method in LifeHackerTeam17Mapper where we can get a list of all the soda's in our database + created a dropdown menu that uses our soda objects as options + Made a controller class for giving the data to the html files to work with

--- src/main/java/app/controllers/LifeHack_Team_17_Controller.java
+++ src/main/java/app/controllers/LifeHack_Team_17_Controller.java
@@ -1,4 +1,22 @@
 package app.controllers;
 
+import app.entities.Soda;
+import app.persistence.ConnectionPool;
+import app.persistence.LifeHackTeam17Mapper;
+import io.javalin.http.Context;
+
+import java.util.List;
+
 public class LifeHack_Team_17_Controller {
+
+    private static ConnectionPool connectionPool;
+
+    public static void home(Context ctx) {
+        LifeHackTeam17Mapper lifeHackTeam17Mapper = new LifeHackTeam17Mapper();
+        List<Soda> sodas = lifeHackTeam17Mapper.getSodaFromDataBase(connectionPool);
+        ctx.attribute("sodas", sodas);
+        ctx.render("calculator.html");
+    }
+
 }
+
---
+++ src/main/java/app/persistence/LifeHackTeam17Mapper.java
@@ -0,0 +1,35 @@
+package app.persistence;
+import app.entities.Soda;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class LifeHackTeam17Mapper {
+
+    private static ConnectionPool connectionPool;
+
+    public ArrayList<Soda> getSodaFromDataBase(ConnectionPool connectionPool) {
+        ArrayList<Soda> sodaList = new ArrayList<>();
+
+        String sql = "SELECT * FROM Soda ORDER BY name";//TODO: Check in later if you need to add ASC or DECS
+        //Creates the connection between server and software
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql)){
+
+            ResultSet rs = ps.executeQuery();
+            //As long as the server still has another row of data we will go through them to get the data and use it to make object with it
+            while(rs.next()) {
+                String name = rs.getString("Name");
+                float liter = rs.getFloat("Liter");
+                sodaList.add(new Soda(name, liter));
+            }
+
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        return sodaList;
+    }
+}
