aebb10868acdadabe9e81f64cc2e7e25e77c79b6
Jon Bertelsen <jobe@cphbusiness.dk>
2025-03-20 00:46:51 +0100

    added frontend files, styling and controller method

--- src/main/java/app/controllers/TeamTeacherController.java
+++ src/main/java/app/controllers/TeamTeacherController.java
@@ -6,10 +6,6 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class TeamTeacherController {
 
 
@@ -18,14 +14,26 @@ public class TeamTeacherController {
 
 
     public static void routes(Javalin app) {
-        app.get("/team-teacher/index", ctx -> teamTeacherFrontHome(ctx));
+        app.get("/philosophers", ctx -> philosophersHome(ctx));
+        app.post("/philosophers", ctx -> ask(ctx));
        // app.get("/ask", ctx -> ctx.render("answer.html"));
 
-    };
-
-    private static void teamTeacherFrontHome(@NotNull Context ctx) {
-        ctx.render("team-teacher/index.html");
     }
+
+    private static void philosophersHome(@NotNull Context ctx) {
+        ctx.render("teamteachers/index.html");
+    }
+
+    private static void ask(@NotNull Context ctx) {
+        // Her søges og hentes fra datamapper - indtil da hardcodes et citat. Tænker også at vi kan gemme billedenavn i db
+        ctx.attribute("quote", "Det uundersøgte liv er ikke værd at leve.");
+        ctx.attribute("philosopher", "Socrates");
+        ctx.attribute("philosopherImage", "socrates.webp");
+        ctx.render("teamteachers/index.html");
+    }
+
+
+
 
     private static void getAnswer(Context ctx, ConnectionPool connectionPool){
 
