c879508761a814fb04534f18056dce2f7c677fbe
Abbas M. Badreddine <abbas.bdr@hotmail.com>
2025-03-20 10:16:45 +0100

    Mappers fixed and tested

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -26,10 +26,6 @@ public class Main {
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args) throws DatabaseException, SQLException {
-        Team1QuestionMapper qm = new Team1QuestionMapper();
-
-        qm.listOfQuestions(connectionPool, 1);
-
 
         // Initializing Javalin and Jetty webserver
 
