681abbc26d11560c18614dcd7b25effb40f73e99
Daniel <Liquidly@live.dk>
2025-03-20 10:46:06 +0100

    Added addProfile method in profilemapper

--- src/main/java/app/persistence/Team4ProfilMapper.java
+++ src/main/java/app/persistence/Team4ProfilMapper.java
@@ -1,6 +1,7 @@
 package app.persistence;
 
 import app.entities.Team4ProfileEntity;
+import app.exceptions.DatabaseException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,7 +19,7 @@ public Team4ProfilMapper(ConnectionPool connectionPool) {
     this.connectionPool = connectionPool;
 }
 
-public List<Team4ProfileEntity> getProfiles() {
+public List<Team4ProfileEntity> getAllProfiles() {
 
     List<Team4ProfileEntity> profiles = new ArrayList<>();
 
@@ -46,7 +47,40 @@ public List<Team4ProfileEntity> getProfiles() {
     return profiles;
 }
 
+public static Team4ProfileEntity addProfile(ConnectionPool connectionPool ,Team4ProfileEntity team4ProfileEntity) throws DatabaseException {
+    String sql = "INSERT INTO profile (color, species, bio, name, age, image, email, password) " +
+                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id";
 
+    try(
+            Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql)
+            ) {
+                ps.setInt(1, team4ProfileEntity.getColor());
+                ps.setInt(2, team4ProfileEntity.getSpecies());
+                ps.setString(3, team4ProfileEntity.getBio());
+                ps.setString(4, team4ProfileEntity.getName());
+                ps.setInt(5, team4ProfileEntity.getAge());
+                ps.setString(6, team4ProfileEntity.getImage());
+                ps.setString(7, team4ProfileEntity.getEmail());
+                ps.setString(8, team4ProfileEntity.getPassword());
+
+                try(var rs = ps.executeQuery()){
+                    if(rs.next()){
+                        int generatedId = rs.getInt("id");
+
+                        return new Team4ProfileEntity(team4ProfileEntity.getColor(), team4ProfileEntity.getSpecies(),
+                                    team4ProfileEntity.getBio(), team4ProfileEntity.getName(), team4ProfileEntity.getAge(),
+                                    team4ProfileEntity.getImage(), team4ProfileEntity.getEmail(), team4ProfileEntity.getPassword(), generatedId );
+
+                    } else {
+                        throw new DatabaseException("Der skete en fejl ved oprettelse af bruger, ingen ID returneret.");
+                    }
+                }
+    } catch (SQLException e){
+        String msg = "Fejl ved tilføjelse af ny bruger. Prøv igen.";
+        throw new DatabaseException(msg, e.getMessage());
+    }
+}
 
 
 
