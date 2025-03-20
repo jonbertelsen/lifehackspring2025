192623abc1e1ceb72556ab3071a2770470792344
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-03-20 15:11:55 +0100

    random jokes

--- src/main/java/app/controllers/Team7Controller.java
+++ src/main/java/app/controllers/Team7Controller.java
@@ -2,22 +2,33 @@ package app.controllers;
 
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.Team7Mapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Random;
 
 public class Team7Controller {
-public static void add(Javalin app, ConnectionPool connectionPool) {
+    public static void add(Javalin app, ConnectionPool connectionPool) {
 
-    app.get("/DadJokes", ctx -> ctx.render("team7_frontpage.html"));
-    app.get("/MomJokes", ctx -> ctx.render("team7_frontpage.html"));
-    app.get("/JesperJokes", ctx -> ctx.render("team7_frontpage.html"));
-    app.get("/NationalJokes", ctx -> ctx.render("team7_frontpage.html"));
-    app.get("/BlondeJokes", ctx -> ctx.render("team7_frontpage.html"));
-    app.get("/DatamatikerJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.get("/DadJokes", ctx -> datJokes(ctx, connectionPool));
+        app.get("/MomJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.get("/JesperJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.get("/NationalJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.get("/BlondeJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.get("/DatamatikerJokes", ctx -> ctx.render("team7_frontpage.html"));
+        app.post("/login", ctx -> ctx.render("team7_frontpage.html"));
+    }
 
-    app.get("/TryLuck", ctx -> ctx.render("team7_try_luck.html"));
-    app.post("/login", ctx -> ctx.render("team7_frontpage.html"));
-}
+    private static void datJokes(@NotNull Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+
+        Random random = new Random();
+        int jokeId = random.nextInt(10);  // lav et tilfældigt tal
+
+        String joke = Team7Mapper.getJoke1ById(connectionPool, jokeId);
+        ctx.attribute("joke", joke);
+        ctx.render("team7_frontpage.html");
+    }
 }
