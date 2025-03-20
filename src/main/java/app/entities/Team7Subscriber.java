652714747597b8a51a974aa56d59cb078fdee01b
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-03-20 12:08:54 +0100

    cha

--- src/main/java/app/entities/Team7Subscriber.java
+++ src/main/java/app/entities/Team7Subscriber.java
@@ -1,7 +1,14 @@
 package app.entities;
 
 public class Team7Subscriber {
+    private int subscriberId;
     private String email;
+
+    public Team7Subscriber(int subscriberId, String email) {
+        this.subscriberId = subscriberId;
+        this.email = email;
+    }
+
 
     public Team7Subscriber(String email) {
         this.email = email;
@@ -13,5 +20,13 @@ public class Team7Subscriber {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Team7Subscriber{" +
+                "subscriberId=" + subscriberId +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
