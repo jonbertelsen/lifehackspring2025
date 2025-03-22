f77435dc0129a23ab4dbcf9a0de6a7d9d8e1a294
EmilDagsberg <cph-ed113@cphbusiness.dk>
2025-03-22 11:17:23 +0100

    ændret dbnavn og added thumbnail. Bør vi good to go

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,7 +16,7 @@ public class Main {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "movie_guesser";
+    private static final String DB = "lifehackspring2025";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
